4d8c02bfcc5d3f7a692b51b242e59ffd75eea4e7
Pete Montgomery <peter.montgomery@ons.gov.uk>
2024-01-16 14:07:39 +0000

    Add Tailwind and recommended extensions

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flex items-center justify-center h-screen">
+    <h1 class="text-3xl font-bold text-gray-500">Holding page</h1>
+</div>
+
+
