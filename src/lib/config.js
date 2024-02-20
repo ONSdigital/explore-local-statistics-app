db11ace2d4f75e78b180ccc13c8adae352952da1
Andrew Hillman <ajhillman_ddj@outlook.com>
2024-02-20 01:38:05 +0000

    Update config.js

--- src/lib/config.js
+++ src/lib/config.js
@@ -20,7 +20,7 @@ export const regions = [
 export let mainChartOptionsArray = [
 	{ id: 0, label: 'Time series', multiYear: 'Yes' },
 	{ id: 1, label: 'Bar chart', multiYear: 'No' },
-	{ id: 2, label: 'Distribution', multiYear: 'No' },
+	{ id: 2, label: 'Beeswarm', multiYear: 'No' },
 	{ id: 3, label: 'Map', multiYear: 'No' },
 	{ id: 4, label: 'Table', multiYear: 'Either' }
 ];
