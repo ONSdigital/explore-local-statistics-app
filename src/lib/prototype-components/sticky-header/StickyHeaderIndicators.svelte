30c75a860d60ded7fdff37f97f583229c992e4fb
Ahmad Barclay <Ahmad.Barclay@ons.gov.uk>
2024-03-07 16:25:27 +0000

    moved indicators sticky header into component

---
+++ src/lib/prototype-components/sticky-header/StickyHeaderIndicators.svelte
@@ -0,0 +1,74 @@
+<script>
+	import ChangeAreas from '$lib/prototype-components/change-areas/ChangeAreas.svelte';
+	import AreaPanel from '$lib/prototype-components/AreaPanel.svelte';
+
+	export let selectionsObject, customLookup, accordionArray;
+</script>
+
+<div class="row-container sticky">
+	<div class="visible-areas-key-container">
+		{#each selectionsObject['indicator-additional-visible'] as area}
+			<AreaPanel
+				{area}
+				bind:chosen={selectionsObject['indicator-additional-chosen']}
+				{customLookup}
+				backgroundColor="color"
+				color="contrast"
+			/>
+		{/each}
+		{#if selectionsObject['indicator-additional-visible'].length === 0}
+			<span class="no-selection">No areas selected</span>
+		{/if}
+	</div>
+	<div class="row-container buttons-container">
+		<ChangeAreas {accordionArray} bind:selectionsObject {customLookup}></ChangeAreas>
+	</div>
+</div>
+
+<style>
+	.no-selection {
+		display: inline-block;
+		color: #222;
+		background: #ddd;
+		padding: 6px;
+		border-radius: 3px;
+		line-height: 20px;
+	}
+
+	.sticky {
+		margin: 0 0 24px;
+		width: 100%;
+		background-color: white;
+		padding: 10px 0px;
+		position: sticky;
+		top: 0px;
+		z-index: 100;
+		display: flex;
+		flex-direction: column;
+		gap: 10px;
+		justify-content: space-between;
+	}
+
+	.row-container {
+		display: flex;
+		flex-direction: row;
+		flex-wrap: nowrap;
+		gap: 4px;
+	}
+
+	.col1 {
+		white-space: nowrap; /* Prevent text in the first column from wrapping */
+		text-align: end;
+	}
+
+	.col2 {
+		display: flex;
+		flex-direction: row;
+		flex-wrap: wrap;
+		align-items: center;
+	}
+
+	.button-container {
+		justify-content: flex-end;
+	}
+</style>
--- src/routes/(app)/datasets/[slug]/+page.svelte
+++ src/routes/(app)/datasets/[slug]/+page.svelte
@@ -15,8 +15,7 @@
 	// import Indicators from '$lib/components/Indicators.svelte';
 	import ContentBlock from '$lib/components/ContentBlock.svelte';
 	import Map from '$lib/viz/Map.svelte';
-	import ChangeAreas from '$lib/prototype-components/change-areas/ChangeAreas.svelte';
-	import AreaPanel from '$lib/prototype-components/AreaPanel.svelte';
+	import StickyHeaderIndicators from '$lib/prototype-components/sticky-header/StickyHeaderIndicators.svelte';
 	import { constructVisibleAreasArray, updateCustomLookup } from '$lib/utils.js';
 
 	export let data;
@@ -172,29 +171,11 @@
 {#if mapData && pivotedData}
 	<NavSections contentsLabel="Explore this indicator" marginTop>
 		<strong class="selected-areas">Selected areas</strong>
-		<div class="row-container sticky">
-			<div class="visible-areas-key-container">
-				{#each selectionsObject['indicator-additional-visible'] as area}
-					<AreaPanel
-						{area}
-						bind:chosen={selectionsObject['indicator-additional-chosen']}
-						customLookup={customLookup['indicator-additional-visible']}
-						backgroundColor="color"
-						color="contrast"
-					/>
-				{/each}
-				{#if selectionsObject['indicator-additional-visible'].length === 0}
-					<span class="no-selection">No areas selected</span>
-				{/if}
-			</div>
-			<div class="row-container buttons-container">
-				<ChangeAreas
-					{accordionArray}
-					bind:selectionsObject
-					customLookup={customLookup['indicator-additional-visible']}
-				></ChangeAreas>
-			</div>
-		</div>
+		<StickyHeaderIndicators
+			bind:selectionsObject
+			{accordionArray}
+			customLookup={customLookup['indicator-additional-visible']}
+		/>
 		<NavSection title="Map">
 			<ContentBlock
 				type="map"
@@ -288,56 +269,8 @@
 	:global(section#map > h2) {
 		margin-top: -6px !important;
 	}
-
-	/* Sticky header CSS */
 	.selected-areas {
 		color: #999;
 		text-transform: uppercase;
-	}
-
-	.no-selection {
-		display: inline-block;
-		color: #222;
-		background: #ddd;
-		padding: 6px;
-		border-radius: 3px;
-		line-height: 20px;
-	}
-
-	.sticky {
-		margin: 0 0 24px;
-		width: 100%;
-		background-color: white;
-		padding: 10px 0px;
-		position: sticky;
-		top: 0px;
-		z-index: 100;
-		display: flex;
-		flex-direction: column;
-		gap: 10px;
-		justify-content: space-between;
-	}
-
-	.row-container {
-		display: flex;
-		flex-direction: row;
-		flex-wrap: nowrap;
-		gap: 4px;
-	}
-
-	.col1 {
-		white-space: nowrap; /* Prevent text in the first column from wrapping */
-		text-align: end;
-	}
-
-	.col2 {
-		display: flex;
-		flex-direction: row;
-		flex-wrap: wrap;
-		align-items: center;
-	}
-
-	.button-container {
-		justify-content: flex-end;
 	}
 </style>
