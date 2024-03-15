4d9aca90d19f9b165a517ef0a2d5b96bc5527f58
James Trimble <jtrim.ons@gmail.com>
2024-03-15 12:03:05 +0000

    Speed up function for reshaping data

--- src/lib/api/getData.ts
+++ src/lib/api/getData.ts
@@ -2,13 +2,24 @@ import { base } from '$app/paths';
 
 type GetDataResult = { kind: 'Success'; chartData: JSON; metadata: JSON } | { kind: 'Failure' };
 
+function fastReshapeIndicatorData(colData) {
+	// On James's MacBook, this takes around 6 ms for all datasets, whereas using code
+	// similar to `reshapeData` takes around 55 ms.
+	return colData.value.map((d: number, i: number) => ({
+		areacd: colData.areacd[i],
+		id: colData.id, // id and code are the same for all objects, so they're not stored as arrays in the JSON file.
+		value: d,
+		lci: colData.lci[i],
+		uci: colData.uci[i],
+		xDomainNumb: colData.xDomainNumb[i]
+	}));
+}
+
 function reshapeData(colData) {
-	const cols = Object.keys(colData).filter((d) => d !== 'id' && d !== 'code');
+	const cols = Object.keys(colData);
 	const rowData = [];
 	for (let i = 0; i < colData[cols[0]].length; i++) {
 		const row: { [key: string]: string | number } = {};
-		// id and code are the same for all objects, so they're not stored as arrays in the JSON file.
-		row.id = colData.id;
 		for (const col of cols) {
 			row[col] = colData[col][i];
 		}
@@ -27,7 +38,9 @@ export const getData = async (fetch: typeof window.fetch): Promise<GetDataResult
 
 		const reshapedData = {};
 		for (const code in dataParsed.combinedDataObjectColumnOriented) {
-			reshapedData[code] = reshapeData(dataParsed.combinedDataObjectColumnOriented[code]);
+			reshapedData[code] = fastReshapeIndicatorData(
+				dataParsed.combinedDataObjectColumnOriented[code]
+			);
 		}
 
 		const clusterData = reshapeData(metadataParsed.clustersLookup.data);
