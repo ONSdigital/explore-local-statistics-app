fe02eff2e3f625ba549f1bf155a27b636b4c25fb
Pete Montgomery <pete.montgomery@ons.gov.uk>
2024-06-17 15:34:47 +0100

    Reorder sitemap slightly

--- scripts/insights/generate-sitemap.ts
+++ scripts/insights/generate-sitemap.ts
@@ -3,65 +3,79 @@ import { slugify } from '../../src/lib/util/url/slugify.ts';
 import { noIndex, essGeocodes } from '../../src/lib/config/geoConfig.ts';
 import { create } from 'xmlbuilder2';
 
-export function generateSitemap (areas, indicators) {
+export function generateSitemap(areas, indicators) {
+	const sitemapData = {
+		areas: areas.filter((d) => !noIndex.includes(d.areacd.slice(0, 3))),
+		'areas-with-indicators': areas.filter((d) => essGeocodes.includes(d.areacd.slice(0, 3))),
+		indicators: indicators.map((d) => d.slug)
+	};
 
+	const intro = `<?xml version="1.0" encoding="utf-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`;
 
-    const sitemapData = {
-        'areas': areas.filter(d => !noIndex.includes(d.areacd.slice(0,3))),
-        'areas-with-indicators': areas.filter(d => essGeocodes.includes(d.areacd.slice(0,3))),
-        'indicators': indicators.map(d => d.slug)
-    }
+	const outro = `</urlset>`;
 
-    const intro = `<?xml version="1.0" encoding="utf-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`
+	const paths: string[] = [];
 
-    const outro = `</urlset>`
+	paths.push('https://explore-local-statistics.beta.ons.gov.uk/');
+	paths.push('https://explore-local-statistics.beta.ons.gov.uk/areas/');
+	paths.push('https://explore-local-statistics.beta.ons.gov.uk/indicators/');
 
-let paths:string[] = []
+	Object.keys(sitemapData).forEach((d) => {
+		if (d == 'areas') {
+			sitemapData[d].forEach((e) => {
+				paths.push(
+					'https://explore-local-statistics.beta.ons.gov.uk/areas/' +
+						e.areacd +
+						'-' +
+						slugify(e.areanm)
+				);
+			});
+		} else if (d == 'areas-with-indicators') {
+			sitemapData[d].forEach((e) => {
+				paths.push(
+					'https://explore-local-statistics.beta.ons.gov.uk/areas/' +
+						e.areacd +
+						'-' +
+						slugify(e.areanm) +
+						'/indicators'
+				);
+			});
+		} else {
+			sitemapData[d].forEach((f) => {
+				paths.push('https://explore-local-statistics.beta.ons.gov.uk/indicators/' + f);
+			});
+		}
+	});
 
-Object.keys(sitemapData).forEach(d => {
-    if (d == "areas") {
-        sitemapData[d].forEach( (e,i) => {
-            paths.push("https://explore-local-statistics.beta.ons.gov.uk/areas/" + e.areacd + "-" + slugify(e.areanm))
-        })
-    } else if (d == "areas-with-indicators") {
-        sitemapData[d].forEach( (e,i) => {
-        paths.push("https://explore-local-statistics.beta.ons.gov.uk/areas/" + e.areacd + "-" + slugify(e.areanm) + "/indicators")
-        })
-    } else {
-        sitemapData[d].forEach( (f) => {
-            paths.push("https://explore-local-statistics.beta.ons.gov.uk/indicators/" + f)
-        })
-    }
-})
+	let content = intro;
 
-paths.push("https://explore-local-statistics.beta.ons.gov.uk/")
-paths.push("https://explore-local-statistics.beta.ons.gov.uk/areas/")
-paths.push("https://explore-local-statistics.beta.ons.gov.uk/indicators/")
+	const date = new Date().toISOString().split('T')[0];
 
-let content = intro;
+	paths.forEach((d) => {
+		content = content.concat(
+			'<url>',
+			'<loc>',
+			d,
+			'</loc>',
+			'<lastmod>',
+			date,
+			'</lastmod>',
+			'</url>'
+		);
+	});
 
-let date = new Date().toISOString().split("T")[0]
+	content = content.concat(outro);
 
-paths.forEach( d => {
-    content = content.concat("<url>",
-        "<loc>", d, "</loc>",
-        "<lastmod>", date, "</lastmod>",
-      "</url>")
-})
+	const doc = create(content).end({ prettyPrint: true });
 
-content = content.concat(outro)
+	// writeJson('./noIndex.json', noIndex);
 
-let doc = create(content).end({ prettyPrint: true })
-
-
-    // writeJson('./noIndex.json', noIndex);
-
-    writeFile('./static/sitemap.xml', doc, err => {
-        if (err) {
-          console.error(err);
-        } else {
-          // file written successfully
-        }
-      });
+	writeFile('./static/sitemap.xml', doc, (err) => {
+		if (err) {
+			console.error(err);
+		} else {
+			// file written successfully
+		}
+	});
 }
