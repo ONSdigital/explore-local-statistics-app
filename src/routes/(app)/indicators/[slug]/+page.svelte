1111a780833da277e1ae443c2ffc1566e990fc7c
MikeLister <131891138+MikeLister@users.noreply.github.com>
2024-07-11 10:23:35 +0100

    Excluding obsolete areas from indicator and area page visualisations and fixing broken postcode search

--- src/routes/(app)/indicators/[slug]/+page.svelte
+++ src/routes/(app)/indicators/[slug]/+page.svelte
@@ -112,7 +112,11 @@
 
 	$: mapData =
 		geoGroup?.codes && chosenXDomainNumbEnd && data.indicator.years.includes(chosenXDomainNumbEnd)
-			? makeMapData(data.chartData, geoGroup?.codes, chosenXDomainNumbEnd)
+			? makeMapData(
+					data.chartData.filter((d) => !d.areanm.includes('(obsolete)')),
+					geoGroup?.codes,
+					chosenXDomainNumbEnd
+				)
 			: { data: [], breaks: [] };
 
 	$: chosenTimePeriodsArray = timePeriodsArray
@@ -127,7 +131,11 @@
 
 	let metadata = data.metadata;
 
-	$: codesForAreasWithData = [...new Set(data.chartData.map((el) => el.areacd))];
+	$: codesForAreasWithData = [
+		...new Set(
+			data.chartData.filter((d) => !d.areanm.includes('(obsolete)')).map((el) => el.areacd)
+		)
+	];
 
 	$: lowerTierLocalAuthoritiesWithDataCodes = metadata.areasGeogLevelObject.lower.filter((el) =>
 		codesForAreasWithData.includes(el)
@@ -536,7 +544,7 @@
 
 				<LineChartContainerIndicatorPage
 					indicator={data.indicator}
-					chartData={data.chartData}
+					chartData={data.chartData.filter((d) => !d.areanm.includes('(obsolete)'))}
 					{selectionsObject}
 					customLookup={customLookup['indicator-additional-visible']}
 					{metadata}
@@ -585,7 +593,7 @@
 			</div>
 			<BarChartContainerIndicatorPage
 				indicator={data.indicator}
-				chartData={data.chartData}
+				chartData={data.chartData.filter((d) => !d.areanm.includes('(obsolete)'))}
 				{selectionsObject}
 				customLookup={customLookup['indicator-additional-visible']}
 				{metadata}
