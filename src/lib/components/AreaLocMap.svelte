847a9d19aeee50bd1737c64c580a0fd3c39e9b17
Ahmad Barclay <Ahmad.Barclay@ons.gov.uk>
2025-08-20 10:45:49 +0100

    changed nav map styles

--- src/lib/components/AreaLocMap.svelte
+++ src/lib/components/AreaLocMap.svelte
@@ -5,27 +5,57 @@
 	export let geometry;
 	export let bounds;
 	export let mapDescription;
+	export let primaryColor = 'rgb(32, 96, 149)';
 </script>
 
-<Map
-	style="{assets}/data/mapstyle.json"
-	location={{ bounds }}
-	interactive={false}
-	options={{ fitBoundsOptions: { padding: 20 } }}
-	{mapDescription}
->
-	{#key geometry}
-		<MapSource id="boundary" type="geojson" data={geometry}>
-			<MapLayer
-				id="boundary-fill"
-				type="fill"
-				paint={{ 'fill-color': 'rgb(17,140,123)', 'fill-opacity': 0.2 }}
-			/>
-			<MapLayer
-				id="boundary-line"
-				type="line"
-				paint={{ 'line-color': 'rgb(17,140,123)', 'line-width': 2 }}
-			/>
-		</MapSource>
-	{/key}
-</Map>
+<div class="map-container">
+	<Map
+		style="{assets}/data/mapstyle-nav.json"
+		location={{ bounds }}
+		interactive={false}
+		options={{ fitBoundsOptions: { padding: { left: 40, top: 40, right: 20, bottom: 20 } } }}
+		{mapDescription}
+	>
+		{#key geometry}
+			<MapSource id="boundary" type="geojson" data={geometry}>
+				<MapLayer
+					id="boundary-fill"
+					type="fill"
+					paint={{ 'fill-color': primaryColor, 'fill-opacity': 0.2 }}
+				/>
+				<MapLayer
+					id="boundary-line"
+					type="line"
+					paint={{ 'line-color': primaryColor, 'line-width': 2 }}
+				/>
+			</MapSource>
+		{/key}
+	</Map>
+</div>
+
+<style>
+	.map-container {
+		position: absolute;
+		top: -55.7px;
+		right: 0;
+		height: calc(100% + 55.7px);
+		width: 35%;
+		min-width: 250px;
+	}
+	.map-container::after {
+		position: absolute;
+		content: ' ';
+		top: 0;
+		bottom: 0;
+		left: 0;
+		right: 0;
+		background-image:
+			linear-gradient(to right, #eaeaea, #00000000 25%),
+			linear-gradient(to bottom, #eaeaea, #00000000 25%);
+	}
+	@media (max-width: 800px) {
+		.map-container {
+			display: none;
+		}
+	}
+</style>
