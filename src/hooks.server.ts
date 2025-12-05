f8cf96101c47415ec35f3ead9683473e0deaf32e
Ahmad Barclay <ahmad.barclay@ons.gov.uk>
2025-12-05 15:54:36 +0000

    custom error pages for unhandled errors

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,4 +1,4 @@
-import type { Handle } from '@sveltejs/kit';
+import type { Handle, HandleServerError } from '@sveltejs/kit';
 import logger from '$lib/logger';
 
 // hook up the logger
@@ -18,4 +18,11 @@ export const handle: Handle = async ({ event, resolve }) => {
 	}
 
 	return response;
+};
+
+export const handleError: HandleServerError = async ({ status, message }) => {
+	return {
+		message: status === 404 ? "Page not found" : message || 'Sorry, there is a problem with the service',
+		errorId: status || 500
+	};
 };
