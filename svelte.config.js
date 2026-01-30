7bdefa6242623d3e214be7acfb96c7a367fed54c
Ahmad Barclay <ahmad.barclay@ons.gov.uk>
2026-01-30 14:58:05 +0000

    always use adapter-node unless environment is vercel

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,7 @@ import nodeAdapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 // custom var to specify the adapter from CI/CD
-const adapter = process.env.SVELTEKIT_ADAPTER === 'node' ? nodeAdapter() : vercelAdapter();
+const adapter = !!process.env.VERCEL ? vercelAdapter() : nodeAdapter();
 // custom var to specify the app base path in production
 const basePath = process.env.SVELTEKIT_BASE_PATH || '';
 // custom var to specify the app assets path in production
