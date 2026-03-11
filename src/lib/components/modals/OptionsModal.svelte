67de6149fedab980694b9e60c504998e3e167592
Nicola Rennie <42338476+nrennie@users.noreply.github.com>
2026-03-11 09:11:47 +0000

    Add toggle for chart type on mobile

--- src/lib/components/modals/OptionsModal.svelte
+++ src/lib/components/modals/OptionsModal.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
-	import { Checkbox } from '@onsvisual/svelte-components';
+	import { Checkbox, ButtonGroup, ButtonGroupItem } from '@onsvisual/svelte-components';
 	import Modal from './Modal.svelte';
 	import RangeSlider from './RangeSlider.svelte';
 	import { cloneState } from './modalHelpers';
 
-	let { data, pageState = $bindable(), hasIntervals = true } = $props();
+	let { data, pageState = $bindable(), hasIntervals = true, mode = 'default' } = $props();
 
 	let _pageState = $state(cloneState(pageState));
 	let formatTick = $derived(pageState?.formatPeriod?.() || ((d) => d));
@@ -35,4 +35,31 @@
 	{#if !hasIntervals}
 		<p class="ons-chart__caption">Note: Confidence intervals not available for this indicator</p>
 	{/if}
+
+	{#if mode === 'default'}
+		<div class="mobile-only">
+			<ButtonGroup
+				name="primary"
+				legend="Choose a chart type"
+				value="beeswarm"
+				visuallyHideLegend
+				bind:selectedChartType={_pageState.selectedChartType}
+			>
+				<ButtonGroupItem value="beeswarm" label="Beeswarm" />
+				<ButtonGroupItem value="line" label="Line chart" />
+			</ButtonGroup>
+		</div>
+	{/if}
 </Modal>
+
+<style>
+	.mobile-only {
+		display: block;
+	}
+
+	@media (min-width: 768px) {
+		.mobile-only {
+			display: none;
+		}
+	}
+</style>
