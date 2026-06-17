dcffe70d8668c7f97c874deb121830c5d0046e2e
Claire Buchan <claire.buchan@ons.gov.uk>
2026-06-17 15:19:14 +0100

    shift bar chart labels and leader lines to account for negative values

--- src/lib/components/charts/Bar.svelte
+++ src/lib/components/charts/Bar.svelte
@@ -160,6 +160,7 @@
 	let nXTicks = $derived(Math.max(2, Math.floor(width / maxTickGap)));
 	let xTicks = $derived(xScale?.ticks?.(nXTicks) || []);
 	let xTicksAreIntegers = $derived(xTicks.every((d) => d % 1 === 0));
+	$inspect(labelLookup);
 </script>
 
 {#snippet bar(b, fill = ONScolours.grey40, opacity = 1, id = '', strokeWidth = 0)}
@@ -232,7 +233,8 @@
 	yPosAdj: number,
 	isDodged: boolean,
 	elbowX: number,
-	height: number
+	height: number,
+	yKey: number
 )}
 	{#if isDodged && yPosAdj !== yPosOrig}
 		<polyline
@@ -240,10 +242,10 @@
 			fill="none"
 			points={(elbowX
 				? [
-						`-14,${yPosAdj + height / 2}`,
+						`${xScale(Math.min(yKey, 0)) - 14},${yPosAdj + height / 2}`,
 						`${elbowX},${yPosAdj + height / 2}`,
 						`${elbowX},${yPosOrig + height / 2}`,
-						`-2,${yPosOrig + height / 2}`
+						`${xScale(Math.min(yKey, 0)) - 2},${yPosOrig + height / 2}`
 					]
 				: [`-14,${yPosOrig + height / 2}`, `-2,${yPosOrig + height / 2}`]
 			).join(' ')}
@@ -362,12 +364,13 @@
 							{@const yPos = labelLookup?.[i]?.y || yScale?.(a[0][idKey])?.y}
 							{@const height = yScale?.(a[0][idKey])?.height || 0}
 							{@const isDodged = labelLookup?.[i]?.isDodged}
+							{@const yVal = xScale(Math.min(0, a[0][yKey]))}
 							<div
 								bind:clientHeight={labelHeights[id]}
 								data-id={id}
 								class="margin-label-selected"
 								style:top="{yPos ? yPos + height / 2 : 0}px"
-								style:left={isDodged ? '-16px' : '-8px'}
+								style:left="{isDodged ? yVal - 16 : yVal - 8}px"
 								style:color={getPaletteColor(i, selectedData.length, 'text')}
 								style:max-width="{leftMargin - 16}px"
 							>
@@ -467,9 +470,9 @@
 						{@const yPosAdj = labelLookup?.[i]?.y || yScale(a[0][idKey]).y}
 						{@const yPosOrig = yScale(a[0][idKey]).y}
 						{@const height = yScale(a[0][idKey]).height}
-						{@const elbowX = xScale(0) - 6 - labelLookup?.[i]?.elbow}
+						{@const elbowX = xScale(Math.min(0, a[0][yKey])) - 6 - labelLookup?.[i]?.elbow}
 						{@const isDodged = labelLookup?.[i]?.isDodged}
-						{@render elbow(yPosOrig, yPosAdj, isDodged, elbowX, height)}
+						{@render elbow(yPosOrig, yPosAdj, isDodged, elbowX, height, a[0][yKey])}
 					{/each}
 				</g>
 			{/if}
